89e477dfffb36f2600d2c6020150c3212324e960
rojasdali <rojasdali@gmail.com>
2019-01-25 16:19:00 -0500

    connected graphql yoga

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,10 @@
+// this file connects to the remote prisma db and gives us th eability to query with js
+
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: process.env.PRISMA_ENDPOINT,
+    secret: process.env.PRISMA_SECRET,
+    debug: false
+})
